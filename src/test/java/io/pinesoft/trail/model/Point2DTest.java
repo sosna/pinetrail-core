039c30a0c7b09e21879f8d95a8ec187f229f8ed2
Xavier Sosnovsky <xso@sosna.ws>
2020-10-15 21:15:47 +0200

    Add support for parameterized tests

--- src/test/java/io/pinesoft/trail/model/Point2DTest.java
+++ src/test/java/io/pinesoft/trail/model/Point2DTest.java
@@ -4,13 +4,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public abstract class Point2DTest<T extends Point2D> {
 
   @Test
   public void createInstance() {
     final double latitude = 47.5913904235;
+    final double longitude = 12.9946215637;
+    final T instance = newInstance(longitude, latitude);
+    assertEquals(longitude, instance.getLongitude(), 0.0);
+    assertEquals(latitude, instance.getLatitude(), 0.0);
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideLatitude")
+  public void valLatitude(final Double latitude) {
     final double longitude = 12.9946215637;
     final T instance = newInstance(longitude, latitude);
     assertEquals(longitude, instance.getLongitude(), 0.0);
@@ -60,4 +76,14 @@ public abstract class Point2DTest<T extends Point2D> {
   }
 
   protected abstract T newInstance(final double longitude, final double latitude);
+
+  private static Stream<Double> provideLatitude() {
+    final List<Double> out =
+        IntStream.range(0, 10)
+            .mapToObj(num -> ThreadLocalRandom.current().nextDouble(-90.0, 90.0))
+            .collect(Collectors.toList());
+    out.add(-90.0);
+    out.add(90.0);
+    return out.stream();
+  }
 }
